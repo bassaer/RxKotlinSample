814f770124b89f740bf344d21ff83b3f41b13ded
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-04-15 21:26:29 +0900

    add ContentFragment

---
+++ app/src/main/java/com/github/bassaer/rxkotlinsample/ContentFragment.kt
@@ -0,0 +1,9 @@
+package layout
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by nakayama on 2018/04/14.
+ */
+class ContentFragment : Fragment() {
+}
--- app/src/main/java/com/github/bassaer/rxkotlinsample/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/rxkotlinsample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.github.bassaer.rxkotlinsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+import layout.ContentFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.container, ContentFragment())
+        transaction.commit()
+
     }
 }
